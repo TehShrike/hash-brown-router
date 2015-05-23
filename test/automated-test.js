66c11986e5733fd17cf2a71791de255eb259836f
Josh Duff <me@JoshDuff.com>
2015-05-23 16:40:15 -0500

    Cleaning up the tests dramatically
    A lot of the jank was due to me not realizing that I was ending my tests when the final planned assertion happened, and not when I was calling t.end.
    
    Also dropped the bit of the testing that was trying to assert that my hashlocation.replace call would work in browsers the way I expected.

--- test/automated-test.js
+++ test/automated-test.js
@@ -1,11 +1,4 @@
-var test = require('tape')
 var hashLocationMockFactory = require('../hash-location-mock')
 var allTests = require('./all-tests')
 
-allTests({
-	add: function(name, fn) {
-		test(name, function(t) {
-			fn(t, hashLocationMockFactory(), t.end.bind(t))
-		})
-	}
-})
+allTests(hashLocationMockFactory())
--- test/browser-test.js
+++ test/browser-test.js
@@ -1,47 +1,9 @@
-var router = require('../')
-var tester = require('./jankety-test-harness')
 var allTests = require('./all-tests')
+var tapBrowserColor = require('tap-browser-color')
+var browserHashLocation = require('../hash-location.js')
 
-allTests(tester)
+var hashLocation = browserHashLocation(window)
 
-tester.add('replacing a url', function(t, locationHash, done) {
-	t.plan(4)
+allTests(hashLocation)
 
-	var route = router()
-
-	var startingHistoryLength = history.length
-
-	function shouldHappenOnce(name, cb) {
-		var happened = false
-		return function() {
-			if (happened) {
-				t.fail(name + ' already happened once')
-			} else {
-				t.pass(name + ' happened')
-				happened = true
-			}
-			cb && cb.apply(null, arguments)
-		}
-	}
-
-	route.add('/initial', shouldHappenOnce('initial route', function() {
-		route.go('/redirect') // history length++
-	}))
-
-	route.add('/redirect', shouldHappenOnce('redirect route', function() {
-		route.replace('/destination')
-	}))
-
-	route.add('/destination', shouldHappenOnce('destination route', function() {
-		var historyDepth = history.length - startingHistoryLength
-		t.equal(historyDepth, 2, 'should be two more items in the history then there were before (was ' + historyDepth + ')')
-		done()
-	}))
-
-	route.setDefault(t.fail.bind(t, 'default route called'))
-
-	route.go('/initial') // history length++
-})
-
-tester.start()
-
+tapBrowserColor()
