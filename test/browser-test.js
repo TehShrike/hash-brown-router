c16b16df9bb591f9c38111d2ad446984d9f93889
Josh Duff <me@JoshDuff.com>
2015-08-22 21:50:33 -0500

    tap-browser-color again why not

--- test/browser-test.js
+++ test/browser-test.js
@@ -1,5 +1,5 @@
 var allTests = require('./all-tests')
-// var tapBrowserColor = require('tap-browser-color')
+var tapBrowserColor = require('tap-browser-color')
 var browserHashLocation = require('../hash-location.js')
 
 var hashLocation = browserHashLocation(window)
